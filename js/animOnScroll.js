337f7ff029536599ba1c5bad6599d368e0082804
CuteBuffy <cutebuffydiscord@gmail.com>
2025-03-22 00:38:16 +0200

    animations

--- js/animOnScroll.js
+++ js/animOnScroll.js
@@ -1,10 +1,11 @@
-const observer = new IntersectionObserver((entries) => {
+const observer = new IntersectionObserver((entries, observer) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      entry.target.classList.add("animated")
+      entry.target.classList.add("animated");
+      observer.unobserve(entry.target);
     }
-  })
-})
+  });
+}, { threshold: 0.2 });
 
 const hiddenElems = document.querySelectorAll(".animate_await")
 hiddenElems.forEach((el) => observer.observe(el))
